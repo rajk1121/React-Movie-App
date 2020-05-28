1b1672261f408343a2b482de545f7183447fd373
rajk1121 <rajat.mishra1121@gmail.com>
2020-05-28 11:24:17 +0530

    Updates

---
+++ precache-manifest.5ae76efa842c438503e1dc1cc17dc8a1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bee1e44877a843624bf3f94873ae9b6",
+    "url": "/React-Movie-App/index.html"
+  },
+  {
+    "revision": "4879be8113a645723bf8",
+    "url": "/React-Movie-App/static/css/2.042a1a68.chunk.css"
+  },
+  {
+    "revision": "11363fede603e7f3f383",
+    "url": "/React-Movie-App/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4879be8113a645723bf8",
+    "url": "/React-Movie-App/static/js/2.6414341d.chunk.js"
+  },
+  {
+    "revision": "11363fede603e7f3f383",
+    "url": "/React-Movie-App/static/js/main.e2173166.chunk.js"
+  },
+  {
+    "revision": "ee672bdd522caa8232f4",
+    "url": "/React-Movie-App/static/js/runtime-main.491ef847.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Movie-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Movie-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Movie-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Movie-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Movie-App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
